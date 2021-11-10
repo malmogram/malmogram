f90cdaafbd6d8b68b33ce5a006d887fc4bb8fc06
malmogram <flradam@gmail.com>
2021-11-10 16:09:02 +0100

    Function for divided line

--- r/Funktioner/Divided-line-graph.R
+++ r/Funktioner/Divided-line-graph.R
@@ -1,57 +1,77 @@
-# Linjediagram med ribbon till nollan (eller något annat värde) med en för om linjen är under noll 
+# Linjediagram med ribbon till nollan (eller något annat värde) med en färg om linjen är under noll 
 # och en annan om linjen är över noll.
+library(tidyverse)
 library(sf)
 
-dat <- tibble(t = 1:200, x = rnorm(200))
+t_max <- 1000
+dat <- tibble(t = 1:t_max) %>% 
+  mutate(x = (1 - t / t_max) * cumsum(rnorm(n())) + t / t_max * rev(cumsum(rnorm(n()))))
 
-xy_minmax <- dat %>% summarise_all(c(min, max)) %>% unlist()
+xy_minmax <- dat %>% 
+  summarise_all(c(min, max)) %>% 
+  unlist()
 
-dat2 <- bind_rows(tibble(t = 1, x = -5),
-                  dat,
-                  tibble(t = c(200, 1), x = c(-5, -5)))
+dat_low <- bind_rows(tibble(t = xy_minmax[1], x = xy_minmax[2] - 1),
+                     dat,
+                     tibble(t = c(xy_minmax[3], xy_minmax[1]), x = c(xy_minmax[2] - 1, xy_minmax[2] - 1)))
 
+dat_high <- bind_rows(tibble(t = xy_minmax[1], x = xy_minmax[4] + 1),
+                      dat,
+                      tibble(t = c(xy_minmax[3], xy_minmax[1]), x = c(xy_minmax[4] + 1, xy_minmax[4] + 1)))
 
-a <- st_polygon(list(as.matrix(dat2))) %>%
+ggplot() + 
+  geom_polygon(aes(t, x), data =dat_low, fill = "#ddddff", col = "black") + 
+  geom_polygon(aes(t, x), data = dat_high, fill = "#ffdddd", col = "black")
+
+dat_low <- st_polygon(list(as.matrix(dat_low))) %>%
+  st_sfc() %>%
+  st_sf()
+dat_high <- st_polygon(list(as.matrix(dat_high))) %>%
   st_sfc() %>%
   st_sf()
 
-dat <- tibble(x = c(1,1,200,200,1,1,1,200,200,1),
-              y = c(-5,0,0,-5,-5,0,5,5,0,0),
-              obj = letters[c(1,1,1,1,1,2,2,2,2,2)])
+dat_rect <- tibble(x = xy_minmax[c(1,1,3,3,1,1,1,3,3,1)],
+                   y = c(xy_minmax[2] - 1,0,0,xy_minmax[2] - 1,xy_minmax[2] - 1,
+                         0,xy_minmax[4] + 1,xy_minmax[4] + 1,0,0),
+                   obj = letters[c(1,1,1,1,1,2,2,2,2,2)])
 
-dat3 <- dat %>%
+dat_rect <- dat_rect %>%
   nest(data = c(x,y)) %>%
   mutate(geo = map(data, ~st_polygon(list(as.matrix(.x)))) %>% st_sfc()) %>%
   st_sf() %>%
   select(-data) %>%
   ungroup()
 
-dat_temp <- a %>%
-  st_difference(dat3) %>%
+dat_temp1 <- dat_low %>%
+  st_difference(dat_rect) %>%
   slice(1) %>%
   st_cast("POLYGON") %>%
-  mutate(id = paste0("up", 1:n())) %>%
-  group_by(id)
+  mutate(id = paste0("up", 1:n()))
 
-dat_temp2 <- a %>%
-  st_difference(dat3) %>%
+dat_temp2 <- dat_high %>%
+  st_difference(dat_rect) %>%
   slice(2) %>%
   st_cast("POLYGON") %>%
-  st_coordinates() %>%
-  as_tibble()
+  mutate(id = paste0("down", 1:n()))
 
-st_coordinates(dat_temp) %>%
-  as_tibble() %>%
-  ggplot(aes(X, Y, group = L2)) +
-  geom_polygon(fill = "darkgreen") +
-  geom_polygon(data = dat_temp2, fill = "red") +
+ggplot() + geom_sf(data = dat_temp1, fill = "red") +
+  geom_sf(data = dat_temp2, fill = "green")
+
+foo <- . %>%  
+  mutate(coord = map(geometry, ~ st_coordinates(.x) %>% as.data.frame() %>% as_tibble())) %>% 
+  as_tibble() %>% 
+  select(-geometry) %>% 
+  unnest(coord)
+
+dat_t <- bind_rows(foo(dat_temp1), foo(dat_temp2)) %>% 
+  mutate(type = substring(id, 1, 1))
+
+ggplot(dat_t, aes(X, Y, group = id, fill = type)) +
+  geom_polygon() +
   coord_polar()
 
-
-dat3 %>% st_difference(a) %>% plot()
-
-
 # Alternative solution. Plot background then add negative space ----
+library(cowplot)
 collection <- list()
 for(i in 1:12){
   dat <- tibble(t = 1:200, x = cumsum(rnorm(200)))
