2e8b120c536b0c3197cf1c3e7248319ab19b080d
malmogram <flradam@gmail.com>
2021-12-01 16:12:57 +0100

    Scrape trees

--- r/temp_trees.R
+++ r/temp_trees.R
@@ -16,4 +16,12 @@ a %>%
   theme(legend.position = "none") +
   coord_equal()
 
-# https://kartor.malmo.se/geoserver/wms?service=WMS&request=GetFeatureInfo&bbox=6140000.0,114000.7,6170000.6,125000.9&layers=malmows:gk_trad_barr_pt,malmows:gk_trad_lov_pt,malmows:gk_trad_exotiska_pt,malmows:gk_trad_nordiska_pt&styles=&typename=malmows:gk_trad_barr_pt,malmows:gk_trad_lov_pt,malmows:gk_trad_exotiska_pt,malmows:gk_trad_nordiska_pt&query_layers=malmows:gk_trad_barr_pt,malmows:gk_trad_lov_pt,malmows:gk_trad_exotiska_pt,malmows:gk_trad_nordiska_pt&info_format=application/json&feature_count=4000000&x=50&y=50&buffer=50&width=100&height=100&srs=EPSG:3008&exceptions=application%2Fvnd.ogc.se_xml&version=1.3.0
+library(rvest)
+a <- "https://kartor.malmo.se/geoserver/wms?service=WMS&request=GetFeatureInfo&bbox=6140000.0,114000.7,6170000.6,125000.9&layers=malmows:gk_trad_barr_pt,malmows:gk_trad_lov_pt,malmows:gk_trad_exotiska_pt,malmows:gk_trad_nordiska_pt&styles=&typename=malmows:gk_trad_barr_pt,malmows:gk_trad_lov_pt,malmows:gk_trad_exotiska_pt,malmows:gk_trad_nordiska_pt&query_layers=malmows:gk_trad_barr_pt,malmows:gk_trad_lov_pt,malmows:gk_trad_exotiska_pt,malmows:gk_trad_nordiska_pt&info_format=application/json&feature_count=4000000&x=10&y=10&buffer=500&width=20&height=20&srs=EPSG:3008&exceptions=application%2Fvnd.ogc.se_xml&version=1.3.0"
+a <- a %>% readLines()
+b <- st_read(a)
+b
+
+library(tidyverse)
+st_coordinates(b) %>% as_tibble() %>% 
+  ggplot(aes(X, Y, color = b$ursprung)) + geom_point(shape = ".") + coord_equal() + cowplot::theme_nothing()
